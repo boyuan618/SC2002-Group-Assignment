42855a728362a5681a4741debeee3a77ac3df8e8
Samyak-Jain-001 <ja.samyak2006@gmail.com>
2025-04-13 09:36:08 +0800

    Update BTOApplication.java

--- src/model/BTOApplication.java
+++ src/model/BTOApplication.java
@@ -1,5 +1,53 @@
 package model;
 
 public class BTOApplication {
+	private Applicant applicant;
+    private BTOProject project;
+    private String status; 
+    private String flatType; 
+
+    public BTOApplication(Applicant applicant, BTOProject project, String status) {
+        this.applicant = applicant;
+        this.project = project;
+        this.status = status;
+        this.flatType = null;
+    }
+
+    public void updateStatus(String newStatus) {
+        if (isValidStatus(newStatus)) {
+            this.status = newStatus;
+        }
+    }
+
+    public void setFlatType(String flatType) {
+        if (flatType.equals("2-Room") || flatType.equals("3-Room")) {
+            this.flatType = flatType;
+        }
+    }
+
+   
+    private boolean isValidStatus(String newStatus) {
+        return newStatus.equals("Pending") ||
+               newStatus.equals("Successful") ||
+               newStatus.equals("Unsuccessful") ||
+               newStatus.equals("Booked") ||
+               newStatus.equals("Withdrawn");
+    }
+    public Applicant getApplicant() {
+        return applicant;
+    }
+
+    public BTOProject getProject() {
+        return project;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getFlatType() {
+        return flatType;
+    }
+}
 
 }
