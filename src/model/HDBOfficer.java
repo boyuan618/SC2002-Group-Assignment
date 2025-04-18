898fde5a2422eee8a7bc5bc959f7ebfe074e4094
ningmeng618 <guboyuan@gmail.com>
2025-04-18 11:37:41 +0800

    updates

--- src/model/HDBOfficer.java
+++ src/model/HDBOfficer.java
@@ -1,5 +1,23 @@
 package model;
 
-public class HDBOfficer {
+public class HDBOfficer extends User {
+    private String assignedProject;
 
+    public HDBOfficer(String nric, String password, int age, String maritalStatus, String assignedProject) {
+        super(nric, password, "HDBOfficer", age, maritalStatus);
+        this.assignedProject = assignedProject;
+    }
+
+    public String getAssignedProject() {
+        return assignedProject;
+    }
+
+    public void setAssignedProject(String assignedProject) {
+        this.assignedProject = assignedProject;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "," + assignedProject;
+    }
 }
