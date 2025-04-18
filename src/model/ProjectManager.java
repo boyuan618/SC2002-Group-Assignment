b0019c2149e038eaf114ba42c31b2c5adb674682
hohohomerrychristmas <152855650+hohohomerrychristmas@users.noreply.github.com>
2025-04-19 01:19:31 +0800

    ProjectManager.java
    Check it out

--- src/model/ProjectManager.java
+++ src/model/ProjectManager.java
@@ -1,5 +1,24 @@
 package model;
 
-public class ProjectManager implements EnquiryInt {
+public class ProjectManager extends HDBManager implements EnquiryInt{ 
+    private BTOProject myProject;
+    private boolean validity; /*idk how they want us to address this*/
+    private boolean visibility;
 
-}
+    public ProjectManager(String a, String b, String c, int d, boolean e, BTOProject f, boolean g, boolean h){
+        super(a,b,c,d,e); myProject=f; validity=g; visibility=h;}
+
+    public void addressOfficerRegistration(String name){
+        if(myProject.getOfficerSlot()==0){
+            System.out.println("No more officer slots left. Registration denied.");
+            return;}
+        myProject.addOfficer(name);
+        return;}
+    
+    public void toggle(){
+        if(visibility==true){
+            visibility=false;
+            return;}
+        else{
+            visibility=true;}}
+    }
